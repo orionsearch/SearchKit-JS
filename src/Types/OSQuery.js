12caf4fe89c600e1bcffc2a76de2ced1832a427e
Arthur Guiot <arguiot@gmail.com>
2019-02-13 17:45:08 +0800

    Added private filter parsing method to OSQuery

--- src/Types/OSQuery.js
+++ src/Types/OSQuery.js
@@ -5,4 +5,20 @@ class OSQuery {
 		this.keys = null
 		this.filters = null
 	}
+	_removeAndParseFilters(text) {
+		const regex = /\S*:\S*/
+		const matches = text.match(regex)
+		let t = text
+		let out = []
+		matches.forEach(filter => {
+			const split = filter.split(":")
+			const name = split[0]
+			const f = split[1]
+			out.push([name, f])
+			// remove text
+			t.replace(filter, "")
+		})
+		out.push(t)
+		return out
+	}
 }

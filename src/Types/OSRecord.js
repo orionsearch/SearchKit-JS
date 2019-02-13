2759b1d86a525b19a81c9ba5e49fa11c16e4465b
Arthur Guiot <arguiot@gmail.com>
2019-02-13 12:06:44 +0800

    Created OSRecord according to API

--- src/Types/OSRecord.js
+++ src/Types/OSRecord.js
@@ -1,4 +1,12 @@
 class OSRecord {
-
+	constructor(data) {
+		this.data = data
+	}
+	get keys() {
+		return Object.keys(this.data)
+	}
+	get values() {
+		return Object.values(this.data)
+	}
 }
 module.exports = OSRecord;

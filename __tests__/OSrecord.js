4a9ed9343574f9d0ac47110a14a028e67a4eaf47
Arthur Guiot <arguiot@gmail.com>
2019-02-14 18:05:22 +0800

    Added better OSRecord tests

--- __tests__/OSrecord.js
+++ __tests__/OSrecord.js
@@ -1,5 +1,13 @@
 const { OSRecord } = require(__testDir + "../index.js")
-
-eye.test("Data", "node",
-	$ => $(new OSRecord({title: "Hello World", content: "blabla"}).keys).Equal(["title", "content"])
+const record = new OSRecord({title: "Hello World", content: "blabla", priv: "test"})
+record.main("title")
+record.secondary("content")
+record.private(["priv"])
+eye.test("Data & Private", "node",
+	$ => $(record.keys).Equal(["title", "content"]),
+	$ => $(record.values).Equal(["Hello World", "blabla"])
+)
+eye.test("Main & Secondary", "node",
+	$ => $(record.main).Equal("title"),
+	$ => $(record.secondary).Equal("content")
 )

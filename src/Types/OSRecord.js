05abb0f54267bde4dc9187511c316a3cf046c8f9
Arthur Guiot <arguiot@gmail.com>
2019-02-14 13:45:28 +0800

    Added main, secondary and private methods to OSRecord

--- src/Types/OSRecord.js
+++ src/Types/OSRecord.js
@@ -1,6 +1,26 @@
 class OSRecord {
 	constructor(data) {
 		this.data = data
+		this._DataCopy = data
+	}
+	main(key) {
+		if (typeof this.data[key] != "undefined") {
+			this.main = key
+		} else {
+			throw `Wrong main key: ${key}`
+		}
+	}
+	secondary(key) {
+		if (typeof this.data[key] != "undefined" && this.main != key) {
+			this.secondary = key
+		} else {
+			throw `Wrong secondary key: ${key}`
+		}
+	}
+	private(keys) {
+		keys.forEach(key => {
+			delete this.data[key]
+		})
 	}
 	get keys() {
 		return Object.keys(this.data)

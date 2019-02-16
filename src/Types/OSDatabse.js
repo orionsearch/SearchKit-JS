d0d074daa44c7bba3ed214706455e7c4b2fad9a5
Arthur Guiot <arguiot@gmail.com>
2019-02-16 18:28:29 +0800

    Working on OSDatabase

--- src/Types/OSDatabse.js
+++ src/Types/OSDatabse.js
@@ -1,6 +1,8 @@
+const { OSRecord } = require("../includes.js")
 class OSDatabase {
 	constructor(cache=new Set()) {
 		this.keywordsCache = cache
+		this._data = []
 	}
 	configure(main, secondary=null, lang="en") {
 		const stopwords = require('stopwords-iso');
@@ -13,18 +15,30 @@ class OSDatabase {
 			return tokens.filter(a => a != "")
 		}
 		this.select().forEach(record => {
+			const keys = new Set()
 			tokenize(record.data[main]).forEach(t => {
 				this.keywordsCache.add(t)
+				keys.add(t)
 			})
 			if (secondary != null) {
 				tokenize(record.data[secondary]).forEach(t => {
 					this.keywordsCache.add(t)
+					keys.add(t)
 				})
+			}
+			if (typeof this._kFunction != "undefined") {
+				this._kFunction(keys)
+			} else {
+				this._keywords(keys)
 			}
 		})
 	}
 	select(contains=null) {
-		this._select(contains)
+		if (typeof this._sFunction != "undefined") {
+			this._sFunction(contains)
+		} else {
+			this._select(contains)
+		}
 	}
 	add(records, main, secondary=null) {
 		const stopwords = require('stopwords-iso');
@@ -37,22 +51,54 @@ class OSDatabase {
 			return tokens.filter(a => a != "")
 		}
 		records.forEach(record => {
+			const keys = new Set()
 			tokenize(record.data[main]).forEach(t => {
 				this.keywordsCache.add(t)
+				keys.add(t)
 			})
 			if (secondary != null) {
 				tokenize(record.data[secondary]).forEach(t => {
 					this.keywordsCache.add(t)
+					keys.add(t)
 				})
 			}
+			if (typeof this._kFunction != "undefined") {
+				this._kFunction(contains)
+			} else {
+				this._keywords(records)
+			}
 		})
-		this._add(records)
+		if (typeof this._aFunction != "undefined") {
+			this._aFunction(contains)
+		} else {
+			this._add(records)
+		}
+	}
+	setPlugin(select, add, keywords) {
+		this._sFunction = select
+		this._aFunction = add
+		this._kFunction = keywords
 	}
 	_select(contains) {
-
+		if (contains == null) {
+			return this._data
+		} else {
+			let out = []
+			this._data.forEach(record => {
+				if (record.data.keywords.has(contains)) {
+					out.push(record)
+				}
+			})
+			return out
+		}
 	}
 	_add(records) {
-
+		records.forEach(record => {
+			this._data.push(record)
+		})
+	}
+	_keywords(keys, record) {
+		record.data["keywords"] = keys
 	}
 }
 

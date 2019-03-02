17bba63e2993bf78287f5c2cae703c13f12167f8
Arthur Guiot <arguiot@gmail.com>
2019-03-02 23:49:37 +0800

    Improved Levenshtein distance algorithm

--- src/main/Search/normal.js
+++ src/main/Search/normal.js
@@ -26,11 +26,12 @@ class OSNormal {
 		return [...set]
 	}
 	getKeywords() {
+		const levenshtein = require("../../Helpers/levenshtein.js")
 		const keywords = Object.keys(this.query.parsed.keywords)
 		const cache = [...this.db.keywordsCache]
 		let out = []
 		keywords.forEach(key => {
-			const scores = cache.map(a => this._levenshtein(a, key))
+			const scores = cache.map(a => levenshtein(a, key))
 
 			function getMin(arr) {
 				let len = arr.length;
@@ -102,53 +103,6 @@ class OSNormal {
 		sorted.forEach(record => {
 			this.callback(record)
 		})
-	}
-
-
-
-
-
-	/* Helpers */
-
-	_levenshtein(a, b) {
-		let alen = a.length;
-		let blen = b.length;
-		if (alen === 0) return blen;
-		if (blen === 0) return alen;
-		let tmp, i, j, prev, val, row, ma, mb, mc, md, bprev;
-
-		if (alen > blen) {
-			tmp = a;
-			a = b;
-			b = tmp;
-		}
-
-		row = new Int8Array(alen + 1);
-		// init the row
-		for (i = 0; i <= alen; i++) {
-			row[i] = i;
-		}
-
-		// fill in the rest
-		for (i = 1; i <= blen; i++) {
-			prev = i;
-			bprev = b[i - 1]
-			for (j = 1; j <= alen; j++) {
-				if (bprev === a[j - 1]) {
-					val = row[j - 1];
-				} else {
-					ma = prev + 1;
-					mb = row[j] + 1;
-					mc = ma - ((ma - mb) & ((mb - ma) >> 7));
-					md = row[j - 1] + 1;
-					val = mc - ((mc - md) & ((md - mc) >> 7));
-				}
-				row[j - 1] = prev;
-				prev = val;
-			}
-			row[alen] = prev;
-		}
-		return row[alen];
 	}
 }
 

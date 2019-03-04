f07fc3e68765c2052a34762efb421f4dc40adb78
Arthur Guiot <arguiot@gmail.com>
2019-03-04 19:47:01 +0800

    Improved regex compilation in tokenize

--- src/Helpers/tokenize.js
+++ src/Helpers/tokenize.js
@@ -4,7 +4,7 @@ const stopwords = require('stopwords-iso');
 function tokenize(text, lang="en") {
 	const stops = stopwords[lang]
 	const t = text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "") // lower case and remove accents
-	const tokens = t.replace(/[^a-zA-Z0-9 ]/g, "")
+	const tokens = t.replace(/[^\w\d\s]/g, "")
 		.replace(/\s{2,}/g, " ")
 		.split(" ")
 	let out = tokens.filter(str => !stops.includes(str) && str != "")
